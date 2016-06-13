3f262a0962a8e607df49dfabf871f1e541faa2c9
fireplex <ampersand2909@gmail.com>
2016-06-13 21:18:52 +0100

    Update main.js

--- firebase/forum/scripts/main.js
+++ firebase/forum/scripts/main.js
@@ -87,7 +87,7 @@ function createPostElement(postId, title, text, author) {
       '<div class="post mdl-cell mdl-cell--12-col ' +
                   'mdl-cell--6-col-tablet mdl-cell--4-col-desktop mdl-grid mdl-grid--no-spacing">' +
         '<div class="mdl-card mdl-shadow--2dp">' +
-          '<div class="mdl-card__title mdl-color--light-blue-600 mdl-color-text--white">' +
+          '<div class="mdl-card__title mdl-color--cyan-500 mdl-color-text--white">' +
             '<h4 class="mdl-card__title-text"></h4>' +
           '</div>' +
           '<div class="header">' +
